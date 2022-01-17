5ff78c969f2cb637b7e5082400b7cab493e9286d
pekosk <rkostyk64@gmail.com>
2022-01-17 16:07:13 +0200

    галерея

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,16 +2,19 @@ import styles from "./ImageGalleryItem.module.css";
 import PropTypes from "prop-types";
 import { memo } from "react";
 
-const ImageGalleryItem = ({ image, onClick }) => (
-  <li className={styles.galleryItem}>
-    <img
-      src={image.webformatURL}
-      alt={image.tags}
-      className={styles.galleryImg}
-      onClick={() => onClick(image.largeImageURL)}
-    />
-  </li>
-);
+const ImageGalleryItem = ({ image, onClick }) => {
+  const openLarge = () => onClick(image.largeImageURL);
+  return (
+    <li className={styles.galleryItem}>
+      <img
+        src={image.webformatURL}
+        alt={image.tags}
+        className={styles.galleryImg}
+        onClick={openLarge}
+      />
+    </li>
+  );
+};
 
 ImageGalleryItem.propTypes = {
   onClick: PropTypes.func.isRequired,
